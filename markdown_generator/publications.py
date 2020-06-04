9f9e0c505bf7557714f3e012ee89db4f78c68553
RCochrane <66378754+RCochrane@users.noreply.github.com>
2020-06-04 18:06:36 -0400

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -57,6 +57,45 @@ for row, item in publications.iterrows():
 !ls https://rcochrane.github.io/_publications/
 !cat https://rcochrane/_publications/2019-06-17-Designer-Sinorhizobium-meliloti-strains-and-multi-functional-vectors-enable-direct-inter-kingdom-DNA-transfer.md
 
+publications
+html_escape_table = {
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&apos;"
+    }
+
+def html_escape(text):
+    """Produce entities within text."""
+    return "".join(html_escape_table.get(c,c) for c in text)
+    import os
+for row, item in publications.iterrows():
+    
+    md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
+    html_filename = str(item.pub_date) + "-" + item.url_slug
+    year = item.pub_date[:4]
+    
+    ## YAML variables
+    
+    md = "---\ntitle: \""   + item.title + '"\n'
+    
+    md += """collection: publications"""
+    
+    md += """\npermalink: /publication/""" + html_filename
+    
+    if len(str(item.excerpt)) > 5:
+        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
+    
+    md += "\ndate: " + str(item.pub_date) 
+    
+    md += "\nvenue: '" + html_escape(item.venue) + "'"
+    
+    if len(str(item.paper_url)) > 5:
+        md += "\npaperurl: '" + item.paper_url + "'"
+    
+    md += "\ncitation: '" + html_escape(item.citation) + "'"
+    
+    md += "\n---"
+    
 if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
     
